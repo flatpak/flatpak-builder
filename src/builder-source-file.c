flatpak/flatpak-builder

commit ac38819106eb039252f0ae45f709896beac70545

Fix file sources that have no sha256 checksums (but have others)
This was missing from the previous commit, and makes file sources
work just like archive sources.

Closes: #78
Approved by: pwithnall

src/builder-source-file.c
@@ -370,9 +370,9 @@ builder_source_file_download (BuilderSource  *source,
       return FALSE;
     }
 
-  if ((self->sha256 == NULL || *self->sha256 == 0) && !is_inline)
+  if (checksums[0] == NULL)
     {
-      g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED, "Sha256 not specified");
+      g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED, "No checksum specified for file source %s", base_name);
       return FALSE;
     }
 

commit 004c1cc9834ab8213ee5f8d85982aa4069f2b6a1

Don't require checksum for inline data
This fixes a regression from ac38819106eb039252f0ae45f709896beac70545

src/builder-source-file.c
@@ -370,7 +370,7 @@ builder_source_file_download (BuilderSource  *source,
       return FALSE;
     }
 
-  if (checksums[0] == NULL)
+  if (checksums[0] == NULL && !is_inline)
     {
       g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED, "No checksum specified for file source %s", base_name);
       return FALSE;