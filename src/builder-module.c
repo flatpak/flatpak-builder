c6a701838f070c9985474d33259ad79273b47ba3
Ryan Gonzalez <rymg19@gmail.com>
2019-08-01 12:32:39 -0500

    Support parsing sources files as yaml
    Fixes #297.

--- src/builder-module.c
+++ src/builder-module.c
@@ -749,7 +749,7 @@ load_sources_from_json (const char *sources_relpath)
     }
 
   builder_manifest_set_demarshal_base_dir (sources_file_dir);
-  sources_root = json_from_string (sources_json, &error);
+  sources_root = builder_json_node_from_data (sources_path, sources_json, &error);
   if (sources_root == NULL)
     {
       g_printerr ("Error parsing %s: %s\n", sources_relpath, error->message);
