flatpak/flatpak-builder

commit a94517b5a10d17e9f412950623f831c8db0a5e30

Run appstream-compose only once
Co-authored-by: Bartłomiej Piotrowski <b@bpiotrowski.pl>

src/builder-context.c
@@ -81,6 +81,8 @@ struct BuilderContext
   gboolean        have_rofiles;
   gboolean        run_tests;
   gboolean        no_shallow_clone;
+  gboolean        opt_export_only;
+  char           *opt_mirror_screenshots_url;
 
   BuilderSdkConfig *sdk_config;
 };
@@ -342,6 +344,32 @@ builder_context_find_in_sources_dirs_va (BuilderContext *self,
     }
 
   return NULL;
+}
+
+void
+builder_context_set_opt_export_only (BuilderContext *self,
+                                     gboolean       opt_export_only)
+{
+  self->opt_export_only = opt_export_only;
+}
+
+gboolean
+builder_context_get_opt_export_only (BuilderContext *self)
+{
+  return self->opt_export_only;
+}
+
+void
+builder_context_set_opt_mirror_screenshots_url (BuilderContext *self,
+                                                const char     *opt_mirror_screenshots_url)
+{
+  self->opt_mirror_screenshots_url = g_strdup(opt_mirror_screenshots_url);
+}
+
+const char *
+builder_context_get_opt_mirror_screenshots_url (BuilderContext *self)
+{
+  return self->opt_mirror_screenshots_url;
 }
 
 GFile *

.gitignore
@@ -103,3 +103,4 @@ system-helper/org.freedesktop.Flatpak.policy
 system-helper/org.freedesktop.Flatpak.rules
 flatpak-bwrap
 py-compile
+libglnx-config.h

src/builder-context.h
@@ -169,6 +169,16 @@ gboolean        builder_context_load_sdk_config (BuilderContext       *self,
                                                  const char           *sdk_path,
                                                  GError              **error);
 
+void            builder_context_set_opt_export_only (BuilderContext *self,
+                                                     gboolean opt_export_only);
+
+gboolean        builder_context_get_opt_export_only (BuilderContext *self);
+
+void            builder_context_set_opt_mirror_screenshots_url (BuilderContext *self,
+                                                                const char *opt_mirror_screenshots_url);
+
+const char *    builder_context_get_opt_mirror_screenshots_url (BuilderContext *self);
+
 BuilderSdkConfig * builder_context_get_sdk_config (BuilderContext *self);
 
 G_DEFINE_AUTOPTR_CLEANUP_FUNC (BuilderContext, g_object_unref)

src/builder-main.c
@@ -528,6 +528,8 @@ main (int    argc,
   builder_context_set_jobs (build_context, opt_jobs);
   builder_context_set_rebuild_on_sdk_change (build_context, opt_rebuild_on_sdk_change);
   builder_context_set_bundle_sources (build_context, opt_bundle_sources);
+  builder_context_set_opt_export_only (build_context, opt_export_only);
+  builder_context_set_opt_mirror_screenshots_url (build_context, opt_mirror_screenshots_url);
 
   git_init_email ();
 
@@ -933,60 +935,6 @@ main (int    argc,
 
   if (!opt_require_changes && !opt_export_only)
     builder_cache_ensure_checkout (cache);
-
-  if (opt_mirror_screenshots_url && !opt_export_only)
-    {
-      g_autofree char *screenshot_subdir = g_strdup_printf ("%s-%s", builder_manifest_get_id (manifest),
-                                                            builder_manifest_get_branch (manifest, build_context));
-      g_autofree char *url = g_build_filename (opt_mirror_screenshots_url, screenshot_subdir, NULL);
-      g_autofree char *xml_relpath = g_strdup_printf ("files/share/app-info/xmls/%s.xml.gz", builder_manifest_get_id (manifest));
-      g_autoptr(GFile) xml = g_file_resolve_relative_path (app_dir, xml_relpath);
-      g_autoptr(GFile) cache = flatpak_build_file (builder_context_get_state_dir (build_context), "screenshots-cache", NULL);
-      g_autoptr(GFile) screenshots = flatpak_build_file (app_dir, "screenshots", NULL);
-      g_autoptr(GFile) screenshots_sub = flatpak_build_file (screenshots, screenshot_subdir, NULL);
-      const char *argv[] = {
-        "appstreamcli", "compose",
-        "--media-baseurl", url,
-        "--media-dir", flatpak_file_get_path_cached (screenshots_sub),
-        flatpak_file_get_path_cached (xml),
-        NULL
-      };
-
-      g_print ("Mirroring screenshots from appdata\n");
-      builder_set_term_title (_("Mirroring screenshots"));
-
-      if (!flatpak_mkdir_p (screenshots, NULL, &error))
-        {
-          g_printerr ("Error creating screenshot dir: %s\n", error->message);
-          return 1;
-        }
-
-      if (g_file_query_exists (xml, NULL))
-        {
-          if (!flatpak_mkdir_p (cache, NULL, &error))
-            {
-              g_printerr ("Error creating screenshot cache dir: %s\n", error->message);
-              return 1;
-            }
-          if (!flatpak_break_hardlink (xml, &error))
-            {
-              g_printerr ("Error mirroring screenshots: %s\n", error->message);
-              return 1;
-            }
-          if (!flatpak_spawnv (NULL,
-                               NULL,
-                               0,
-                               &error,
-                               argv,
-                               NULL))
-            {
-              g_printerr ("Error mirroring screenshots: %s\n", error->message);
-              return 1;
-            }
-        }
-
-      g_print ("Saved screenshots in %s\n", flatpak_file_get_path_cached (screenshots));
-    }
 
   if (!opt_build_only &&
       (opt_repo || opt_install) &&

src/builder-manifest.c
@@ -2425,16 +2425,16 @@ static GFile *
 builder_manifest_find_appdata_file (BuilderManifest *self,
                                     GFile           *app_root)
 {
-  /* We order these so that share/appdata/XXX.appdata.xml if found
+  /* We order these so that share/metainfo/$FLATPAK_ID.metainfo.xml is found
      first, as this is the target name, and apps may have both, which will
      cause issues with the rename. */
   const char *extensions[] = {
-    ".appdata.xml",
     ".metainfo.xml",
+    ".appdata.xml",
   };
   const char *dirs[] = {
-    "share/appdata",
     "share/metainfo",
+    "share/appdata",
   };
   g_autoptr(GFile) source = NULL;
 
@@ -2533,16 +2533,15 @@ builder_manifest_cleanup (BuilderManifest *self,
       appdata_source = builder_manifest_find_appdata_file (self, app_root);
       if (appdata_source)
         {
-          /* We always use the old name / dir, in case the runtime has older appdata tools */
-          g_autoptr(GFile) appdata_dir = g_file_resolve_relative_path (app_root, "share/appdata");
-          g_autofree char *appdata_basename = g_strdup_printf ("%s.appdata.xml", self->id);
+          g_autoptr(GFile) appdata_dir = g_file_resolve_relative_path (app_root, "share/metainfo");
+          g_autofree char *appdata_basename = g_strdup_printf ("%s.metainfo.xml", self->id);
 
           appdata_file = g_file_get_child (appdata_dir, appdata_basename);
 
           if (!g_file_equal (appdata_source, appdata_file))
             {
               g_autofree char *src_basename = g_file_get_basename (appdata_source);
-              g_print ("Renaming %s to share/appdata/%s\n", src_basename, appdata_basename);
+              g_print ("Renaming %s to share/metainfo/%s\n", src_basename, appdata_basename);
 
               if (!flatpak_mkdir_p (appdata_dir, NULL, error))
                 return FALSE;
@@ -2768,19 +2767,61 @@ builder_manifest_cleanup (BuilderManifest *self,
 
       if (self->appstream_compose && appdata_file != NULL)
         {
-          g_autofree char *components_arg = g_strdup_printf ("--components=%s", self->id);
-          g_autofree char *app_root_path = g_file_get_path (app_root);
+          g_autofree char *origin = g_strdup_printf ("--origin=%s",
+                                                     builder_manifest_get_id (self));
+          g_autofree char *components_arg = g_strdup_printf ("--components=%s",
+                                                             self->id);
+          const char *app_root_path = flatpak_file_get_path_cached (app_root);
           g_autofree char *result_root_arg = g_strdup_printf ("--result-root=%s", app_root_path);
+          g_autoptr(GFile) xml_dir = flatpak_build_file (app_root, "share/app-info/xmls", NULL);
+          g_autoptr(GFile) icon_out = flatpak_build_file (app_root, "share/app-info/icons/flatpak", NULL);
+          g_autofree char *data_dir = g_strdup_printf ("--data-dir=%s",
+                                                       flatpak_file_get_path_cached (xml_dir));
+          g_autofree char *icon_dir = g_strdup_printf ("--icons-dir=%s",
+                                                       flatpak_file_get_path_cached (icon_out));
+          const char *opt_mirror_screenshots_url = builder_context_get_opt_mirror_screenshots_url (context);
+          gboolean opt_export_only = builder_context_get_opt_export_only (context);
 
-          g_print ("Running appstreamcli compose\n");
-          if (!appstreamcli_compose (error,
-                                     "--prefix=/",
-                                     "--origin=flatpak",
-                                     result_root_arg,
-                                     components_arg,
-                                     app_root_path,
-                                     NULL))
-            return FALSE;
+          if (opt_mirror_screenshots_url && !opt_export_only)
+            {
+              g_autofree char *screenshot_subdir = g_strdup_printf ("%s-%s",
+                                                                    builder_manifest_get_id (self),
+                                                                    builder_manifest_get_branch (self, context));
+              g_autofree char *url = g_build_filename (opt_mirror_screenshots_url, screenshot_subdir, NULL);
+              g_autoptr(GFile) screenshots = flatpak_build_file (app_root, "screenshots", NULL);
+              g_autofree char *arg_base_url = g_strdup_printf ("--media-baseurl=%s", url);
+              g_autofree char *arg_media_dir =  g_strdup_printf ("--media-dir=%s",
+                                                                 flatpak_file_get_path_cached (screenshots));
+
+              g_print ("Running appstreamcli compose\n");
+              g_print ("Saving screenshots in %s\n", flatpak_file_get_path_cached (screenshots));
+              if (!appstreamcli_compose (error,
+                                         "--prefix=/",
+                                         origin,
+                                         arg_base_url,
+                                         arg_media_dir,
+                                         result_root_arg,
+                                         data_dir,
+                                         icon_dir,
+                                         components_arg,
+                                         app_root_path,
+                                         NULL))
+              return FALSE;
+            }
+          else
+            {
+              g_print ("Running appstreamcli compose\n");
+              if (!appstreamcli_compose (error,
+                                         "--prefix=/",
+                                         origin,
+                                         result_root_arg,
+                                         data_dir,
+                                         icon_dir,
+                                         components_arg,
+                                         app_root_path,
+                                         NULL))
+                return FALSE;
+            }
         }
 
       if (!builder_context_disable_rofiles (context, error))

commit c7275a6dc05589e8a9254b375aa9b7107260da76

builder-context: Free opt_mirror_screenshots_url
```
==103576==ERROR: LeakSanitizer: detected memory leaks

Direct leak of 26 byte(s) in 1 object(s) allocated from:
    #0 0x7fad03ce6f2b in malloc (/lib64/libasan.so.8+0xe6f2b) (BuildId: 25975f766867e9e604dc5a71a8befeaed3301942)
    #1 0x7fad03af0ca9 in g_malloc (/lib64/libglib-2.0.so.0+0x47ca9) (BuildId: 7460891e8cbad6f228b9a8d1dfc92d12bb8a8a7d)
    #2 0x7fad03b094d9 in g_strdup (/lib64/libglib-2.0.so.0+0x604d9) (BuildId: 7460891e8cbad6f228b9a8d1dfc92d12bb8a8a7d)
    #3 0x000000414aee in g_strdup_inline /usr/include/glib-2.0/glib/gstrfuncs.h:321
    #4 0x000000414aee in builder_context_set_opt_mirror_screenshots_url ../src/builder-context.c:372
    #5 0x00000043e023 in main ../src/builder-main.c:617
    #6 0x7fad02c105b4 in __libc_start_call_main (/lib64/libc.so.6+0x35b4) (BuildId: 88c03969b4319fc0140734fe863825ec4a1cdb0d)
    #7 0x7fad02c10667 in __libc_start_main@@GLIBC_2.34 (/lib64/libc.so.6+0x3667) (BuildId: 88c03969b4319fc0140734fe863825ec4a1cdb0d)
    #8 0x000000402a04 in _start (/home/bbhtt/Git/github/flatpak/flatpak-builder/_build/src/flatpak-builder+0x402a04) (BuildId: ceef36bddc16e96f2e16edb20dd70b7103a7d44e)
```

Fixes: a94517b5 ("Run appstream-compose only once")

src/builder-context.c
@@ -130,6 +130,7 @@ builder_context_finalize (GObject *object)
   g_free (self->default_branch);
   g_free (self->state_subdir);
   g_free (self->stop_at);
+  g_free (self->opt_mirror_screenshots_url);
   g_strfreev (self->cleanup);
   g_strfreev (self->cleanup_platform);
   glnx_release_lock_file(&self->rofiles_file_lock);