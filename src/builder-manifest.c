flatpak/flatpak-builder

commit 7a710f6cc5b6e27a5c0a0eff376bbaccfe46c097

Issue #1 - Rename mime files and icons
- Added rename-mime-file and rename-mime-icons.

https://github.com/flatpak/flatpak-builder/issues/1

src/builder-manifest.c
@@ -1,6 +1,7 @@
 /* builder-manifest.c
  *
  * Copyright (C) 2015 Red Hat, Inc
+ * Copyright © 2023 GNOME Foundation Inc.
  *
  * This file is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
@@ -17,6 +18,7 @@
  *
  * Authors:
  *       Alexander Larsson <alexl@redhat.com>
+ *       Hubert Figuière <hub@figuiere.net>
  */
 
 #include "config.h"
@@ -90,8 +92,10 @@ struct BuilderManifest
   char          **tags;
   char           *rename_desktop_file;
   char           *rename_appdata_file;
+  char           *rename_mime_file;
   char           *appdata_license;
   char           *rename_icon;
+  char          **rename_mime_icons;
   gboolean        copy_icon;
   char           *desktop_file_name_prefix;
   char           *desktop_file_name_suffix;
@@ -160,8 +164,10 @@ enum {
   PROP_TAGS,
   PROP_RENAME_DESKTOP_FILE,
   PROP_RENAME_APPDATA_FILE,
+  PROP_RENAME_MIME_FILE,
   PROP_APPDATA_LICENSE,
   PROP_RENAME_ICON,
+  PROP_RENAME_MIME_ICONS,
   PROP_COPY_ICON,
   PROP_DESKTOP_FILE_NAME_PREFIX,
   PROP_DESKTOP_FILE_NAME_SUFFIX,
@@ -212,8 +218,10 @@ builder_manifest_finalize (GObject *object)
   g_strfreev (self->tags);
   g_free (self->rename_desktop_file);
   g_free (self->rename_appdata_file);
+  g_free (self->rename_mime_file);
   g_free (self->appdata_license);
   g_free (self->rename_icon);
+  g_free (self->rename_mime_icons);
   g_free (self->desktop_file_name_prefix);
   g_free (self->desktop_file_name_suffix);
 
@@ -450,12 +458,20 @@ builder_manifest_get_property (GObject    *object,
       g_value_set_string (value, self->rename_appdata_file);
       break;
 
+    case PROP_RENAME_MIME_FILE:
+      g_value_set_string (value, self->rename_mime_file);
+      break;
+
     case PROP_APPDATA_LICENSE:
       g_value_set_string (value, self->appdata_license);
       break;
 
     case PROP_RENAME_ICON:
       g_value_set_string (value, self->rename_icon);
+      break;
+
+    case PROP_RENAME_MIME_ICONS:
+      g_value_set_boxed (value, self->rename_mime_icons);
       break;
 
     case PROP_DESKTOP_FILE_NAME_PREFIX:
@@ -711,6 +727,11 @@ builder_manifest_set_property (GObject      *object,
       self->rename_appdata_file = g_value_dup_string (value);
       break;
 
+    case PROP_RENAME_MIME_FILE:
+      g_free (self->rename_mime_file);
+      self->rename_mime_file = g_value_dup_string (value);
+      break;
+
     case PROP_APPDATA_LICENSE:
       g_free (self->appdata_license);
       self->appdata_license = g_value_dup_string (value);
@@ -719,6 +740,12 @@ builder_manifest_set_property (GObject      *object,
     case PROP_RENAME_ICON:
       g_free (self->rename_icon);
       self->rename_icon = g_value_dup_string (value);
+      break;
+
+    case PROP_RENAME_MIME_ICONS:
+      tmp = self->rename_mime_icons;
+      self->rename_mime_icons = g_strdupv (g_value_get_boxed (value));
+      g_strfreev (tmp);
       break;
 
     case PROP_DESKTOP_FILE_NAME_PREFIX:
@@ -1041,6 +1068,13 @@ builder_manifest_class_init (BuilderManifestClass *klass)
                                                         NULL,
                                                         G_PARAM_READWRITE));
   g_object_class_install_property (object_class,
+                                   PROP_RENAME_MIME_FILE,
+                                   g_param_spec_string ("rename-mime-file",
+                                                        "",
+                                                        "",
+                                                        NULL,
+                                                        G_PARAM_READWRITE));
+  g_object_class_install_property (object_class,
                                    PROP_APPDATA_LICENSE,
                                    g_param_spec_string ("appdata-license",
                                                         "",
@@ -1053,6 +1087,13 @@ builder_manifest_class_init (BuilderManifestClass *klass)
                                                         "",
                                                         "",
                                                         NULL,
+                                                        G_PARAM_READWRITE));
+  g_object_class_install_property (object_class,
+                                   PROP_RENAME_MIME_ICONS,
+                                   g_param_spec_boxed ("rename-mime-icons",
+                                                        "",
+                                                        "",
+                                                        G_TYPE_STRV,
                                                         G_PARAM_READWRITE));
   g_object_class_install_property (object_class,
                                    PROP_COPY_ICON,
@@ -1865,8 +1906,10 @@ builder_manifest_checksum_for_cleanup (BuilderManifest *self,
   builder_cache_checksum_strv (cache, self->cleanup_commands);
   builder_cache_checksum_str (cache, self->rename_desktop_file);
   builder_cache_checksum_str (cache, self->rename_appdata_file);
+  builder_cache_checksum_str (cache, self->rename_mime_file);
   builder_cache_checksum_str (cache, self->appdata_license);
   builder_cache_checksum_str (cache, self->rename_icon);
+  builder_cache_checksum_strv (cache, self->rename_mime_icons);
   builder_cache_checksum_boolean (cache, self->copy_icon);
   builder_cache_checksum_str (cache, self->desktop_file_name_prefix);
   builder_cache_checksum_str (cache, self->desktop_file_name_suffix);
@@ -2169,7 +2212,13 @@ command (GFile      *app_dir,
   return builder_maybe_host_spawnv (NULL, NULL, 0, error, (const char * const *)args->pdata, NULL);
 }
 
-typedef gboolean (*ForeachFileFunc) (BuilderManifest *self,
+typedef struct {
+  const char *rename_icon;
+  gboolean    copy_icon;
+  const char *id;
+} ForeachFile;
+
+typedef gboolean (*ForeachFileFunc) (ForeachFile     *self,
                                      int              source_parent_fd,
                                      const char      *source_name,
                                      const char      *full_dir,
@@ -2180,7 +2229,7 @@ typedef gboolean (*ForeachFileFunc) (BuilderManifest *self,
                                      GError         **error);
 
 static gboolean
-foreach_file_helper (BuilderManifest *self,
+foreach_file_helper (ForeachFile     *self,
                      ForeachFileFunc  func,
                      int              source_parent_fd,
                      const char      *source_name,
@@ -2242,7 +2291,7 @@ foreach_file_helper (BuilderManifest *self,
 }
 
 static gboolean
-foreach_file (BuilderManifest *self,
+foreach_file (ForeachFile     *self,
               ForeachFileFunc  func,
               gboolean        *found,
               GFile           *root,
@@ -2257,15 +2306,16 @@ foreach_file (BuilderManifest *self,
 }
 
 static gboolean
-rename_icon_cb (BuilderManifest *self,
-                int              source_parent_fd,
-                const char      *source_name,
-                const char      *full_dir,
-                const char      *rel_dir,
-                struct stat     *stbuf,
-                gboolean        *found,
-                int              depth,
-                GError         **error)
+_rename_icon (ForeachFile     *self,
+              int              source_parent_fd,
+              const char      *source_name,
+              const char      *full_dir,
+              const char      *rel_dir,
+              gboolean         prefix,
+              struct stat     *stbuf,
+              gboolean        *found,
+              int              depth,
+              GError         **error)
 {
   if (g_str_has_prefix (source_name, self->rename_icon))
     {
@@ -2275,8 +2325,12 @@ rename_icon_cb (BuilderManifest *self,
            g_str_has_prefix (source_name + strlen (self->rename_icon), "-symbolic.")))
         {
           const char *extension = source_name + strlen (self->rename_icon);
-          g_autofree char *new_name = g_strconcat (self->id, extension, NULL);
+          g_autofree char *new_name;
           int res;
+          if (!prefix)
+            new_name = g_strconcat (self->id, extension, NULL);
+          else
+            new_name = g_strconcat (self->id, ".", source_name, NULL);
 
           *found = TRUE;
 
@@ -2305,6 +2359,40 @@ rename_icon_cb (BuilderManifest *self,
     }
 
   return TRUE;
+}
+
+static gboolean
+rename_icon_cb (ForeachFile     *self,
+                int              source_parent_fd,
+                const char      *source_name,
+                const char      *full_dir,
+                const char      *rel_dir,
+                struct stat     *stbuf,
+                gboolean        *found,
+                int              depth,
+                GError         **error)
+{
+  return _rename_icon (self,
+                       source_parent_fd, source_name,
+                       full_dir, rel_dir, FALSE,
+                       stbuf, found, depth, error);
+}
+
+static gboolean
+rename_mime_icon_cb (ForeachFile     *self,
+                     int              source_parent_fd,
+                     const char      *source_name,
+                     const char      *full_dir,
+                     const char      *rel_dir,
+                     struct stat     *stbuf,
+                     gboolean        *found,
+                     int              depth,
+                     GError         **error)
+{
+  return _rename_icon (self,
+                       source_parent_fd, source_name,
+                       full_dir, rel_dir, TRUE,
+                       stbuf, found, depth, error);
 }
 
 static int
@@ -2457,6 +2545,147 @@ builder_manifest_find_appdata_file (BuilderManifest *self,
         }
     }
   return NULL;
+}
+
+/* Perform `rename-mime-file` */
+static gboolean
+_cleanup_rename_mime_file (BuilderManifest *self, GFile *app_root,
+                           GError **error)
+{
+  g_autoptr(GFile) applications_dir = g_file_resolve_relative_path (app_root, "share/mime/packages");
+  g_autoptr(GFile) src = g_file_get_child (applications_dir, self->rename_mime_file);
+  g_autofree char *mime_basename = g_strdup_printf ("%s.xml", self->id);
+  g_autoptr(GFile) dest = g_file_get_child (applications_dir, mime_basename);
+
+  g_print ("Renaming %s to %s\n", self->rename_mime_file, mime_basename);
+  if (!g_file_move (src, dest, 0, NULL, NULL, NULL, error))
+    return FALSE;
+
+  return TRUE;
+}
+
+static gboolean
+_rename_mime_icon (BuilderManifest *self, const char *rename_icon,
+                   GFile *icons_dir,
+                   GError **error)
+{
+  gboolean found_icon = FALSE;
+
+  ForeachFile renamer;
+  renamer.rename_icon = rename_icon;
+  renamer.copy_icon = self->copy_icon;
+  renamer.id = self->id;
+  if (!foreach_file (&renamer, rename_mime_icon_cb, &found_icon, icons_dir, error))
+    return FALSE;
+
+  if (!found_icon)
+    {
+      g_autofree char *icon_path = g_file_get_path (icons_dir);
+      g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED,
+                   "icon %s not found below %s",
+                   rename_icon, icon_path);
+      return FALSE;
+    }
+
+  return TRUE;
+}
+
+/* Rename the icons inside the mime_file.
+ *
+ * The problem is that they may not be in the file as there is an
+ * automatic mapping of mimetypes with icon name, so we have to add
+ * them to he mime file in that case.
+ */
+static gboolean
+_cleanup_mime_file_rename_icons (char **rename_mime_icons,
+                                 GFile *mime_file,
+                                 const char *id, GError **error)
+{
+  FlatpakXml *n_root;
+  g_autoptr(FlatpakXml) xml_root = NULL;
+  g_autoptr(GInputStream) in = NULL;
+  g_autoptr(GString) new_contents = NULL;
+
+  in = (GInputStream *) g_file_read (mime_file, NULL, error);
+  if (!in)
+    return FALSE;
+  xml_root = flatpak_xml_parse (in, FALSE, NULL, error);
+  if (!xml_root)
+    return FALSE;
+
+  n_root = flatpak_xml_find (xml_root, "mime-info", NULL);
+
+  for (char **current = rename_mime_icons; *current; current++)
+    {
+      FlatpakXml *n_type = NULL;
+      while ((n_type = flatpak_xml_find_next (n_root, "mime-type", n_type, NULL)))
+        {
+          FlatpakXml *n_icon = flatpak_xml_find (n_type, "icon", NULL);
+          if (!n_icon)
+            n_icon = flatpak_xml_find (n_type, "generic-icon", NULL);
+          if (n_icon)
+            {
+              const gchar *icon_name = flatpak_xml_attribute (n_icon, "name");
+              if (g_strcmp0 (*current, icon_name) == 0)
+                {
+                  g_autofree gchar *renamed_icon = g_strdup_printf ("%s.%s", id, icon_name);
+                  flatpak_xml_set_attribute (n_icon, "name", renamed_icon);
+                }
+            }
+          else
+            {
+              g_autofree char* mimetype = g_strdup(flatpak_xml_attribute (n_type, "type"));
+              /* Convert the mime type to an icon name. */
+              for (char *p = mimetype; *p; p++)
+                if (*p == '/')
+                  *p = '-';
+
+              if (g_strcmp0(mimetype, *current) == 0)
+                {
+                  g_autofree gchar *renamed_icon = g_strdup_printf ("%s.%s", id, *current);
+                  const gchar *attrs[] = { "name", NULL };
+                  const gchar *vals[] = { renamed_icon, NULL };
+                  n_icon = flatpak_xml_new_with_attributes ("icon", attrs, vals);
+                  flatpak_xml_add (n_type, g_steal_pointer (&n_icon));
+                }
+            }
+        }
+    }
+
+  new_contents = g_string_new ("");
+  flatpak_xml_to_string (xml_root, new_contents);
+  if (!g_file_set_contents (flatpak_file_get_path_cached (mime_file),
+                            new_contents->str,
+                            new_contents->len,
+                            error))
+    return FALSE;
+
+  return TRUE;
+}
+
+/* Perform `rename-mime-icons` */
+static gboolean
+_cleanup_rename_mime_icons (BuilderManifest *self, GFile *app_root,
+                            GError **error)
+{
+  g_autoptr(GFile) icons_dir = g_file_resolve_relative_path (app_root, "share/icons");
+  g_autoptr(GFile) mime_dir;
+  g_autofree char *mime_basename;
+  g_autoptr(GFile) mime_file;
+
+  for (char **current = self->rename_mime_icons; *current; current++)
+    if (!_rename_mime_icon (self, *current, icons_dir, error))
+      return FALSE;
+
+  mime_dir = g_file_resolve_relative_path (app_root, "share/mime/packages");
+  mime_basename = g_strdup_printf ("%s.xml", self->id);
+  mime_file = g_file_get_child (mime_dir, mime_basename);
+
+  if (!_cleanup_mime_file_rename_icons (self->rename_mime_icons, mime_file,
+                                        self->id, error))
+    return FALSE;
+
+  return TRUE;
 }
 
 gboolean
@@ -2625,12 +2854,19 @@ builder_manifest_cleanup (BuilderManifest *self,
             }
         }
 
+      if (self->rename_mime_file != NULL)
+        if (!_cleanup_rename_mime_file (self, app_root, error))
+          return FALSE;
+
       if (self->rename_icon)
         {
           gboolean found_icon = FALSE;
           g_autoptr(GFile) icons_dir = g_file_resolve_relative_path (app_root, "share/icons");
-
-          if (!foreach_file (self, rename_icon_cb, &found_icon, icons_dir, error))
+          ForeachFile renamer;
+          renamer.rename_icon = self->rename_icon;
+          renamer.copy_icon = self->copy_icon;
+          renamer.id = self->id;
+          if (!foreach_file (&renamer, rename_icon_cb, &found_icon, icons_dir, error))
             return FALSE;
 
           if (!found_icon)
@@ -2642,6 +2878,10 @@ builder_manifest_cleanup (BuilderManifest *self,
               return FALSE;
             }
         }
+
+      if (self->rename_mime_icons)
+        if (!_cleanup_rename_mime_icons (self, app_root, error))
+          return FALSE;
 
       if (self->rename_icon ||
           self->desktop_file_name_prefix ||

doc/flatpak-manifest.xml
@@ -262,8 +262,16 @@
                     <listitem><para>Any appdata (metainfo) file with this name will be renamed to a name based on id during the cleanup phase.</para></listitem>
                 </varlistentry>
                 <varlistentry>
+                    <term><option>rename-mime-file</option> (string)</term>
+                    <listitem><para>Any mimetypes file with this name will me renamed to a name based on id during the cleanup phase. (since 1.4.0)</para></listitem>
+                </varlistentry>
+                <varlistentry>
                     <term><option>rename-icon</option> (string)</term>
                     <listitem><para>Any icon with this name will be renamed to a name based on id during the cleanup phase. Note that this is the icon name, not the full filenames, so it should not include a filename extension. </para></listitem>
+                </varlistentry>
+                <varlistentry>
+                    <term><option>rename-mime-icons</option> (array of string)</term>
+                    <listitem><para>Any mime icons with any of these names will be renamed to a name prefixed with id during the cleanup phase. Note that this is the icon name, not the full filenames, so it should not include a filename extension. (since 1.4.0)</para></listitem>
                 </varlistentry>
                 <varlistentry>
                     <term><option>appdata-license</option> (string)</term>

src/builder-manifest.h
@@ -1,5 +1,6 @@
 /*
  * Copyright © 2015 Red Hat, Inc
+ * Copyright © 2023 GNOME Foundation Inc.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -16,6 +17,7 @@
  *
  * Authors:
  *       Alexander Larsson <alexl@redhat.com>
+ *       Hubert Figuière <hub@figuiere.net>
  */
 
 #ifndef __BUILDER_MANIFEST_H__

src/builder-utils.c
@@ -1,6 +1,7 @@
 /* builder-utils.c
  *
  * Copyright (C) 2015 Red Hat, Inc
+ * Copyright © 2023 GNOME Foundation Inc.
  *
  * This file is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
@@ -17,6 +18,7 @@
  *
  * Authors:
  *       Alexander Larsson <alexl@redhat.com>
+ *       Hubert Figuière <hub@figuiere.net>
  */
 
 #include "config.h"
@@ -1410,6 +1412,20 @@ flatpak_xml_new (const gchar *element_name)
 }
 
 FlatpakXml *
+flatpak_xml_new_with_attributes (const gchar *element_name,
+                                 const gchar **attribute_names,
+                                 const gchar **attribute_values)
+{
+  FlatpakXml *node = g_new0 (FlatpakXml, 1);
+
+  node->element_name = g_strdup (element_name);
+  node->attribute_names = g_strdupv ((char **) attribute_names);
+  node->attribute_values = g_strdupv ((char **) attribute_values);
+
+  return node;
+}
+
+FlatpakXml *
 flatpak_xml_new_text (const gchar *text)
 {
   FlatpakXml *node = g_new0 (FlatpakXml, 1);
@@ -1441,9 +1457,9 @@ xml_start_element (GMarkupParseContext *context,
   XmlData *data = user_data;
   FlatpakXml *node;
 
-  node = flatpak_xml_new (element_name);
-  node->attribute_names = g_strdupv ((char **) attribute_names);
-  node->attribute_values = g_strdupv ((char **) attribute_values);
+  node = flatpak_xml_new_with_attributes (element_name,
+                                          attribute_names,
+                                          attribute_values);
 
   flatpak_xml_add (data->current, node);
   data->current = node;
@@ -1515,6 +1531,31 @@ flatpak_xml_free (FlatpakXml *node)
   g_free (node);
 }
 
+const gchar *
+flatpak_xml_attribute (FlatpakXml *node, const gchar* name)
+{
+  if (node->attribute_names)
+    for (int i = 0; node->attribute_names[i] != NULL; i++)
+      if (g_strcmp0(node->attribute_names[i], name) == 0)
+        return node->attribute_values[i];
+
+  return NULL;
+}
+
+gboolean
+flatpak_xml_set_attribute (FlatpakXml *node, const gchar* name, const gchar* value)
+{
+  if (node->attribute_names)
+    for (int i = 0; node->attribute_names[i] != NULL; i++)
+      if (g_strcmp0(node->attribute_names[i], name) == 0)
+        {
+          g_free (node->attribute_values[i]);
+          node->attribute_values[i] = g_strdup (value);
+          return TRUE;
+        }
+
+  return FALSE;
+}
 
 void
 flatpak_xml_to_string (FlatpakXml *node, GString *res)
@@ -1535,9 +1576,11 @@ flatpak_xml_to_string (FlatpakXml *node, GString *res)
             {
               for (i = 0; node->attribute_names[i] != NULL; i++)
                 {
-                  g_string_append_printf (res, " %s=\"%s\"",
-                                          node->attribute_names[i],
-                                          node->attribute_values[i]);
+                  g_autofree char* attr =
+                    g_markup_printf_escaped (" %s=\"%s\"",
+                                             node->attribute_names[i],
+                                             node->attribute_values[i]);
+                  g_string_append (res, attr);
                 }
             }
           if (node->first_child == NULL)
@@ -1595,11 +1638,28 @@ flatpak_xml_find (FlatpakXml  *node,
                   const char  *type,
                   FlatpakXml **prev_child_out)
 {
+  return flatpak_xml_find_next (node, type, NULL, prev_child_out);
+}
+
+FlatpakXml *
+flatpak_xml_find_next (FlatpakXml  *node,
+                       const char  *type,
+                       FlatpakXml  *sibling,
+                       FlatpakXml **prev_child_out)
+{
   FlatpakXml *child = NULL;
   FlatpakXml *prev_child = NULL;
 
-  child = node->first_child;
-  prev_child = NULL;
+  if (!sibling)
+    {
+      child = node->first_child;
+      prev_child = NULL;
+    }
+  else
+    {
+      child = sibling->next_sibling;
+      prev_child = sibling;
+    }
   while (child != NULL)
     {
       FlatpakXml *next = child->next_sibling;

src/builder-utils.h
@@ -1,5 +1,6 @@
 /*
  * Copyright © 2015 Red Hat, Inc
+ * Copyright © 2023 GNOME Foundation Inc.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -16,6 +17,7 @@
  *
  * Authors:
  *       Alexander Larsson <alexl@redhat.com>
+ *       Hubert Figuière <hub@figuiere.net>
  */
 
 #ifndef __BUILDER_UTILS_H__
@@ -169,6 +171,9 @@ struct FlatpakXml
 };
 
 FlatpakXml *flatpak_xml_new (const gchar *element_name);
+FlatpakXml *flatpak_xml_new_with_attributes (const gchar *element_name,
+                                             const gchar **attribute_names,
+                                             const gchar **attribute_values);
 FlatpakXml *flatpak_xml_new_text (const gchar *text);
 void       flatpak_xml_add (FlatpakXml *parent,
                             FlatpakXml *node);
@@ -177,13 +182,26 @@ FlatpakXml *flatpak_xml_parse (GInputStream *in,
                                gboolean      compressed,
                                GCancellable *cancellable,
                                GError      **error);
+const gchar *flatpak_xml_attribute (FlatpakXml  *node,
+                                    const gchar *name);
+gboolean flatpak_xml_set_attribute (FlatpakXml  *node,
+                                    const gchar *name,
+                                    const gchar *value);
 void       flatpak_xml_to_string (FlatpakXml *node,
                                   GString    *res);
 FlatpakXml *flatpak_xml_unlink (FlatpakXml *node,
                                 FlatpakXml *prev_sibling);
+/** Find the first child of `type`. */
 FlatpakXml *flatpak_xml_find (FlatpakXml  *node,
                               const char  *type,
                               FlatpakXml **prev_child_out);
+/** Find the next child from sibling. If `sibling` is NULL, it's
+ *  equivalant to calling `flatpak_xml_find()`.
+ */
+FlatpakXml *flatpak_xml_find_next (FlatpakXml  *node,
+                                   const char  *type,
+                                   FlatpakXml  *sibling,
+                                   FlatpakXml **prev_child_out);
 
 GBytes *   flatpak_read_stream (GInputStream *in,
                                 gboolean      null_terminate,

tests/Hello.desktop
@@ -3,5 +3,5 @@ Version=1.0
 Type=Application
 Name=Hello
 Exec=hello2.sh
-Icon=org.test.Hello2
+Icon=Hello
 MimeType=x-test/Hello;

tests/Hello.xml
@@ -1,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
+  <mime-type type="application/x-hello">
+    <comment>Hello Document</comment>
+    <magic>
+      <match type="string" value="&lt;?xml" offset="0">
+        <match type="string" value="&lt;hello" offset="0:128"/>
+      </match>
+    </magic>
+    <glob pattern="*.hello"/>
+  </mime-type>
+  <mime-type type="application/x-goodbye">
+    <comment>Goodbye Document</comment>
+    <glob pattern="*.goodbye"/>
+    <icon name="application-x-goodbye" />
+  </mime-type>
+</mime-info>

commit b1f55a8719bfe34666d92dcfcb3d90c0f77eb0bf

builder-manifest: Use g_strfreev for rename_mime_icons
```
==18321==ERROR: LeakSanitizer: detected memory leaks
Direct leak of 42 byte(s) in 2 object(s) allocated from:
    #0 0x556f6e32c9f8 in malloc (/__w/flatpak-builder/flatpak-builder/builddir/src/flatpak-builder+0x16b9f8) (BuildId: 3472a3ffe85b3b8ab8d9411dbe38470094bd25fb)
    #1 0x7f3790b50679 in g_malloc (/usr/lib/x86_64-linux-gnu/libglib-2.0.so.0+0x67679) (BuildId: 2426ee6f828a10bf4da963a4c474752c33742359)
    #2 0x7f3790b6f309 in g_strdup (/usr/lib/x86_64-linux-gnu/libglib-2.0.so.0+0x86309) (BuildId: 2426ee6f828a10bf4da963a4c474752c33742359)
    #3 0x7f3790b717d4 in g_strdupv (/usr/lib/x86_64-linux-gnu/libglib-2.0.so.0+0x887d4) (BuildId: 2426ee6f828a10bf4da963a4c474752c33742359)
    #4 0x556f6e3e764a in builder_manifest_set_property /__w/flatpak-builder/flatpak-builder/builddir/../src/builder-manifest.c:747:33
    #5 0x7f37908cfb47  (/usr/lib/x86_64-linux-gnu/libgobject-2.0.so.0+0x27b47) (BuildId: 0f9dc57097a22c42f8b9d339c75528f2205b3639)
    #6 0x7f37908d2b11 in g_object_setv (/usr/lib/x86_64-linux-gnu/libgobject-2.0.so.0+0x2ab11) (BuildId: 0f9dc57097a22c42f8b9d339c75528f2205b3639)
    #7 0x7f37908d2d90 in g_object_set_property (/usr/lib/x86_64-linux-gnu/libgobject-2.0.so.0+0x2ad90) (BuildId: 0f9dc57097a22c42f8b9d339c75528f2205b3639)
    #8 0x556f6e470db1 in builder_serializable_set_property /__w/flatpak-builder/flatpak-builder/builddir/../src/builder-utils.c:1450:3
    #9 0x7f3790892ee9  (/usr/lib/x86_64-linux-gnu/libjson-glib-1.0.so.0+0x16ee9) (BuildId: 52797195f0452e7f82834063cf716b7a6489cc00)
    #10 0x556f6e4697d0 in builder_gobject_from_data /__w/flatpak-builder/flatpak-builder/builddir/../src/builder-utils.c:583:10
    #11 0x556f6e3b18da in main /__w/flatpak-builder/flatpak-builder/builddir/../src/builder-main.c:790:34
    #12 0x7f37901a3600  (/usr/lib/x86_64-linux-gnu/libc.so.6+0x2a600) (BuildId: 90ebd03ae9d9f42b23b4eb82fdf70352cf744198)
    #13 0x7f37901a3717 in __libc_start_main (/usr/lib/x86_64-linux-gnu/libc.so.6+0x2a717) (BuildId: 90ebd03ae9d9f42b23b4eb82fdf70352cf744198)
    #14 0x556f6e287454 in _start (/__w/flatpak-builder/flatpak-builder/builddir/src/flatpak-builder+0xc6454) (BuildId: 3472a3ffe85b3b8ab8d9411dbe38470094bd25fb)
Direct leak of 14 byte(s) in 1 object(s) allocated from:
    #0 0x556f6e32c9f8 in malloc (/__w/flatpak-builder/flatpak-builder/builddir/src/flatpak-builder+0x16b9f8) (BuildId: 3472a3ffe85b3b8ab8d9411dbe38470094bd25fb)
    #1 0x7f3790b50679 in g_malloc (/usr/lib/x86_64-linux-gnu/libglib-2.0.so.0+0x67679) (BuildId: 2426ee6f828a10bf4da963a4c474752c33742359)
    #2 0x7f3790b6f3fe in g_strndup (/usr/lib/x86_64-linux-gnu/libglib-2.0.so.0+0x863fe) (BuildId: 2426ee6f828a10bf4da963a4c474752c33742359)
    #3 0x556f6e47e1b5 in xml_text /__w/flatpak-builder/flatpak-builder/builddir/../src/builder-utils.c:1580:16
    #4 0x7f3790b524ed in g_markup_parse_context_parse (/usr/lib/x86_64-linux-gnu/libglib-2.0.so.0+0x694ed) (BuildId: 2426ee6f828a10bf4da963a4c474752c33742359)
    #5 0x556f6e47a0ab in flatpak_xml_parse /__w/flatpak-builder/flatpak-builder/builddir/../src/builder-utils.c:1807:12
    #6 0x556f6e3c4115 in builder_manifest_cleanup /__w/flatpak-builder/flatpak-builder/builddir/../src/builder-manifest.c:2857:26
    #7 0x556f6e3b34ec in main /__w/flatpak-builder/flatpak-builder/builddir/../src/builder-main.c:1036:12
    #8 0x7f37901a3600  (/usr/lib/x86_64-linux-gnu/libc.so.6+0x2a600) (BuildId: 90ebd03ae9d9f42b23b4eb82fdf70352cf744198)
    #9 0x7f37901a3717 in __libc_start_main (/usr/lib/x86_64-linux-gnu/libc.so.6+0x2a717) (BuildId: 90ebd03ae9d9f42b23b4eb82fdf70352cf744198)
    #10 0x556f6e287454 in _start (/__w/flatpak-builder/flatpak-builder/builddir/src/flatpak-builder+0xc6454) (BuildId: 3472a3ffe85b3b8ab8d9411dbe38470094bd25fb)
SUMMARY: AddressSanitizer: 56 byte(s) leaked in 3 allocation(s).
```

Fixes: 7a710f6c

src/builder-manifest.c
@@ -216,12 +216,12 @@ builder_manifest_finalize (GObject *object)
   g_strfreev (self->inherit_extensions);
   g_strfreev (self->inherit_sdk_extensions);
   g_strfreev (self->tags);
+  g_strfreev (self->rename_mime_icons);
   g_free (self->rename_desktop_file);
   g_free (self->rename_appdata_file);
   g_free (self->rename_mime_file);
   g_free (self->appdata_license);
   g_free (self->rename_icon);
-  g_free (self->rename_mime_icons);
   g_free (self->desktop_file_name_prefix);
   g_free (self->desktop_file_name_suffix);
 