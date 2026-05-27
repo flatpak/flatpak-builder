flatpak/flatpak-builder

commit 050feb12946a78caee1a0c9aaf669f1bf2f99a37

Add --install argument
This allows you to build and install the result in a single operation.
If a --repo is given the app is installed from there, otherwise we
build-export it to the flatpak-builder cache repo, which already has
all the objects that are needed in it, so this will not increase
disk-use.

Closes: #81
Approved by: alexlarsson

src/builder-main.c
@@ -57,6 +57,7 @@ static gboolean opt_allow_missing_runtimes;
 static gboolean opt_sandboxed;
 static gboolean opt_rebuild_on_sdk_change;
 static gboolean opt_skip_if_unchanged;
+static gboolean opt_install;
 static char *opt_state_dir;
 static char *opt_from_git;
 static char *opt_from_git_branch;
@@ -123,6 +124,7 @@ static GOptionEntry entries[] = {
   { "from-git", 0, 0, G_OPTION_ARG_STRING, &opt_from_git, "Get input files from git repo", "URL"},
   { "from-git-branch", 0, 0, G_OPTION_ARG_STRING, &opt_from_git_branch, "Branch to use in --from-git", "BRANCH"},
   { "mirror-screenshots-url", 0, 0, G_OPTION_ARG_STRING, &opt_mirror_screenshots_url, "Download and rewrite screenshots to match this url", "URL"},
+  { "install", 0, 0, G_OPTION_ARG_NONE, &opt_install, "Install if build succeeds", NULL},
   { "install-deps-from", 0, 0, G_OPTION_ARG_STRING, &opt_install_deps_from, "Install build dependencies from this remote", "REMOTE"},
   { "install-deps-only", 0, 0, G_OPTION_ARG_NONE, &opt_install_deps_only, "Stop after installing dependencies"},
   { "user", 0, 0, G_OPTION_ARG_NONE, &opt_user, "Install dependencies in user installations", NULL },
@@ -248,6 +250,50 @@ do_export (BuilderContext *build_context,
   return TRUE;
 }
 
+static gboolean
+do_install (BuilderContext *build_context,
+            const gchar    *repodir,
+            const gchar    *id,
+            const gchar    *branch,
+            GError        **error)
+{
+  g_autofree char *ref = NULL;
+
+  g_autoptr(GPtrArray) args = NULL;
+  g_autoptr(GSubprocess) subp = NULL;
+
+  args = g_ptr_array_new_with_free_func (g_free);
+  g_ptr_array_add (args, g_strdup ("flatpak"));
+  g_ptr_array_add (args, g_strdup ("install"));
+
+  if (opt_user)
+    g_ptr_array_add (args, g_strdup ("--user"));
+  else if (opt_installation)
+    g_ptr_array_add (args, g_strdup_printf ("--installation=%s", opt_installation));
+  else
+    g_ptr_array_add (args, g_strdup ("--system"));
+
+  g_ptr_array_add (args, g_strdup ("--subpath="));
+
+  ref = flatpak_build_untyped_ref (id, branch,
+                                   builder_context_get_arch (build_context));
+
+  g_ptr_array_add (args, g_strdup (repodir));
+  g_ptr_array_add (args, g_strdup (ref));
+
+  g_ptr_array_add (args, NULL);
+
+  subp =
+    g_subprocess_newv ((const gchar * const *) args->pdata,
+                       G_SUBPROCESS_FLAGS_NONE,
+                       error);
+
+  if (subp == NULL ||
+      !g_subprocess_wait_check (subp, NULL, error))
+    return FALSE;
+
+  return TRUE;
+}
 
 int
 main (int    argc,
@@ -282,6 +328,7 @@ main (int    argc,
   g_autoptr(FlatpakContext) arg_context = NULL;
   g_autoptr(FlatpakTempDir) cleanup_manifest_dir = NULL;
   g_autofree char *manifest_basename = NULL;
+  const char *export_repo = NULL;
   int i, first_non_arg, orig_argc;
   int argnr;
   char *p;
@@ -792,19 +839,25 @@ main (int    argc,
       g_print ("Saved screenshots in %s\n", flatpak_file_get_path_cached (screenshots));
     }
 
-  if (!opt_build_only && opt_repo && (opt_export_only || builder_cache_has_checkout (cache)))
+  if (!opt_build_only &&
+      (opt_repo || opt_install) &&
+      (opt_export_only || builder_cache_has_checkout (cache)))
     {
       g_autoptr(GFile) debuginfo_metadata = NULL;
       g_autoptr(GFile) sourcesinfo_metadata = NULL;
       g_auto(GStrv) exclude_dirs = builder_manifest_get_exclude_dirs (manifest);
       GList *l;
 
+      export_repo = opt_repo;
+      if (opt_install && export_repo == NULL)
+        export_repo = flatpak_file_get_path_cached (builder_context_get_cache_dir (build_context));
+
       g_print ("Exporting %s to repo\n", builder_manifest_get_id (manifest));
       builder_set_term_title (_("Exporting to repository"));
 
       if (!do_export (build_context, &error,
                       FALSE,
-                      opt_repo, app_dir_path, exclude_dirs, builder_manifest_get_branch (manifest),
+                      export_repo, app_dir_path, exclude_dirs, builder_manifest_get_branch (manifest),
                       builder_manifest_get_collection_id (manifest),
                       "--exclude=/lib/debug/*",
                       "--include=/lib/debug/app",
@@ -837,7 +890,7 @@ main (int    argc,
           files_arg = g_strconcat (builder_context_get_build_runtime (build_context) ? "--files=usr" : "--files=files",
                                    "/share/runtime/locale/", NULL);
           if (!do_export (build_context, &error, TRUE,
-                          opt_repo, app_dir_path, NULL, builder_manifest_get_branch (manifest),
+                          export_repo, app_dir_path, NULL, builder_manifest_get_branch (manifest),
                           builder_manifest_get_collection_id (manifest),
                           metadata_arg,
                           files_arg,
@@ -856,7 +909,7 @@ main (int    argc,
           g_print ("Exporting %s to repo\n", debug_id);
 
           if (!do_export (build_context, &error, TRUE,
-                          opt_repo, app_dir_path, NULL, builder_manifest_get_branch (manifest),
+                          export_repo, app_dir_path, NULL, builder_manifest_get_branch (manifest),
                           builder_manifest_get_collection_id (manifest),
                           "--metadata=metadata.debuginfo",
                           builder_context_get_build_runtime (build_context) ? "--files=usr/lib/debug" : "--files=files/lib/debug",
@@ -886,7 +939,7 @@ main (int    argc,
                                        builder_extension_get_directory (e));
 
           if (!do_export (build_context, &error, TRUE,
-                          opt_repo, app_dir_path, NULL, builder_manifest_get_branch (manifest),
+                          export_repo, app_dir_path, NULL, builder_manifest_get_branch (manifest),
                           builder_manifest_get_collection_id (manifest),
                           metadata_arg, files_arg,
                           NULL))
@@ -904,7 +957,7 @@ main (int    argc,
           g_print ("Exporting %s to repo\n", sources_id);
 
           if (!do_export (build_context, &error, TRUE,
-                          opt_repo, app_dir_path, NULL, builder_manifest_get_branch (manifest),
+                          export_repo, app_dir_path, NULL, builder_manifest_get_branch (manifest),
                           builder_manifest_get_collection_id (manifest),
                           "--metadata=metadata.sources",
                           "--files=sources",
@@ -923,7 +976,7 @@ main (int    argc,
           g_print ("Exporting %s to repo\n", platform_id);
 
           if (!do_export (build_context, &error, TRUE,
-                          opt_repo, app_dir_path, NULL, builder_manifest_get_branch (manifest),
+                          export_repo, app_dir_path, NULL, builder_manifest_get_branch (manifest),
                           builder_manifest_get_collection_id (manifest),
                           "--metadata=metadata.platform",
                           "--files=platform",
@@ -956,7 +1009,7 @@ main (int    argc,
           metadata_arg = g_strdup_printf ("--metadata=%s", name);
           files_arg = g_strconcat ("--files=platform/share/runtime/locale/", NULL);
           if (!do_export (build_context, &error, TRUE,
-                          opt_repo, app_dir_path, NULL, builder_manifest_get_branch (manifest),
+                          export_repo, app_dir_path, NULL, builder_manifest_get_branch (manifest),
                           builder_manifest_get_collection_id (manifest),
                           metadata_arg,
                           files_arg,
@@ -965,6 +1018,19 @@ main (int    argc,
               g_printerr ("Export failed: %s\n", error->message);
               return 1;
             }
+        }
+    }
+
+  if (opt_install)
+    {
+      g_assert (export_repo != NULL);
+      if (!do_install (build_context, export_repo,
+                       builder_manifest_get_id (manifest),
+                       builder_manifest_get_branch (manifest),
+                       &error))
+        {
+          g_printerr ("Install failed: %s\n", error->message);
+          return 1;
         }
     }
 

doc/flatpak-builder.xml
@@ -547,6 +547,14 @@
             </varlistentry>
 
             <varlistentry>
+                <term><option>--install</option></term>
+
+                <listitem><para>
+                    When the build is finished, install the result locally.
+                </para></listitem>
+            </varlistentry>
+
+            <varlistentry>
                 <term><option>--user</option></term>
 
                 <listitem><para>

commit 2300bd5c8241a0698bbb3799a2ceaf476c53d33f

builder-main: Drop redundant g_strdup on install refs
```
==116984==ERROR: LeakSanitizer: detected memory leaks

Direct leak of 128 byte(s) in 1 object(s) allocated from:
    #0 0x7f8fe7ee5e4b in realloc.part.0 (/lib64/libasan.so.8+0xe5e4b) (BuildId: 25975f766867e9e604dc5a71a8befeaed3301942)
    #1 0x7f8fe7cf1b4a in g_realloc (/lib64/libglib-2.0.so.0+0x48b4a) (BuildId: 7460891e8cbad6f228b9a8d1dfc92d12bb8a8a7d)
    #2 0x7f8fe7d19505 in g_string_expand (/lib64/libglib-2.0.so.0+0x70505) (BuildId: 7460891e8cbad6f228b9a8d1dfc92d12bb8a8a7d)
    #3 0x7f8fe7d195a1 in g_string_sized_new (/lib64/libglib-2.0.so.0+0x705a1) (BuildId: 7460891e8cbad6f228b9a8d1dfc92d12bb8a8a7d)
    #4 0x7f8fe7ccf974 in g_build_path_va (/lib64/libglib-2.0.so.0+0x26974) (BuildId: 7460891e8cbad6f228b9a8d1dfc92d12bb8a8a7d)
    #5 0x7f8fe7cd028c in g_build_filename (/lib64/libglib-2.0.so.0+0x2728c) (BuildId: 7460891e8cbad6f228b9a8d1dfc92d12bb8a8a7d)
    #6 0x000000422535 in flatpak_build_untyped_ref ../src/builder-flatpak-utils.c:431
    #7 0x00000043be0f in do_install ../src/builder-main.c:378
    #8 0x000000444c33 in main ../src/builder-main.c:1291
    #9 0x7f8fe6e105b4 in __libc_start_call_main (/lib64/libc.so.6+0x35b4) (BuildId: 88c03969b4319fc0140734fe863825ec4a1cdb0d)
    #10 0x7f8fe6e10667 in __libc_start_main@@GLIBC_2.34 (/lib64/libc.so.6+0x3667) (BuildId: 88c03969b4319fc0140734fe863825ec4a1cdb0d)
    #11 0x000000402a04 in _start (/home/bbhtt/Git/github/flatpak/flatpak-builder/_build/src/flatpak-builder+0x402a04) (BuildId: 0c3009d54ce681e58b74da540d51b88f4d932cd6)
```

Fixes: 050feb12 ("Add --install argument")

src/builder-main.c
@@ -350,8 +350,6 @@ do_install (BuilderContext *build_context,
             const gchar    *branch,
             GError        **error)
 {
-  g_autofree char *ref = NULL;
-
   g_autoptr(GPtrArray) args = NULL;
 
   args = g_ptr_array_new_with_free_func (g_free);
@@ -375,9 +373,8 @@ do_install (BuilderContext *build_context,
   for (size_t i = 0; i < manifest_ids->len; i++)
     {
       const gchar *id = g_ptr_array_index (manifest_ids, i);
-      ref = flatpak_build_untyped_ref (id, branch,
-                                       builder_context_get_arch (build_context));
-      g_ptr_array_add (args, g_strdup (ref));
+      g_ptr_array_add (args, flatpak_build_untyped_ref (id, branch,
+                                                        builder_context_get_arch (build_context)));
     }
 
   g_ptr_array_add (args, NULL);