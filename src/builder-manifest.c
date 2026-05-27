flatpak/flatpak-builder

commit d9f5a5e7e49d36ce5fa086f274ced20da9f8f336

manifest: Don't emit empty id in `provides` when renaming
Followup from https://github.com/flatpak/flatpak-builder/pull/579

Signed-off-by: Hubert Figuière <hub@figuiere.net>

src/builder-manifest.c
@@ -2806,9 +2806,6 @@ builder_manifest_cleanup (BuilderManifest *self,
               FlatpakXml *n_id;
               FlatpakXml *n_root;
               FlatpakXml *n_text;
-              FlatpakXml *n_provides = NULL;
-              FlatpakXml *n_provides_id = NULL;
-              FlatpakXml *id_text = NULL;
               g_autofree char *old_id = NULL;
               g_autoptr(FlatpakXml) xml_root = NULL;
               g_autoptr(GInputStream) in = NULL;
@@ -2841,16 +2838,23 @@ builder_manifest_cleanup (BuilderManifest *self,
                     }
                 }
 
-              n_provides = flatpak_xml_find (n_root, "provides", NULL);
-              if (!n_provides)
+              if (old_id)
                 {
-                  n_provides = flatpak_xml_new ("provides");
-                  flatpak_xml_add (n_root, n_provides);
+                  FlatpakXml *n_provides = NULL;
+                  FlatpakXml *n_provides_id = NULL;
+                  FlatpakXml *id_text = NULL;
+
+                  n_provides = flatpak_xml_find (n_root, "provides", NULL);
+                  if (!n_provides)
+                    {
+                      n_provides = flatpak_xml_new ("provides");
+                      flatpak_xml_add (n_root, n_provides);
+                    }
+                  n_provides_id = flatpak_xml_new ("id");
+                  id_text = flatpak_xml_new_text (g_steal_pointer (&old_id));
+                  flatpak_xml_add (n_provides_id, id_text);
+                  flatpak_xml_add (n_provides, n_provides_id);
                 }
-              n_provides_id = flatpak_xml_new ("id");
-              id_text = flatpak_xml_new_text (g_steal_pointer (&old_id));
-              flatpak_xml_add (n_provides_id, id_text);
-              flatpak_xml_add (n_provides, n_provides_id);
 
               /* replace any optional launchable */
               n_id = flatpak_xml_find (n_root, "launchable", NULL);

commit 68f3f8ed69948e7046da6603a3a607f5e8fea840

builder-manifest: Fix leak when preserving old ID in metainfo
```
=18260==ERROR: LeakSanitizer: detected memory leaks
Direct leak of 14 byte(s) in 1 object(s) allocated from:
 #0 0x7fe6b28e060f in malloc ../../../../src/libsanitizer/asan/asan_malloc_linux.cpp:67
 #1 0x7fe6b26c4679 in g_malloc (/usr/lib/x86_64-linux-gnu/libglib-2.0.so.0+0x67679) (BuildId: 2426ee6f828a10bf4da963a4c474752c33742359)
 #2 0x7fe6b26e33fe in g_strndup (/usr/lib/x86_64-linux-gnu/libglib-2.0.so.0+0x863fe) (BuildId: 2426ee6f828a10bf4da963a4c474752c33742359)
 #3 0x5609bfa7ec3d in xml_text ../src/builder-utils.c:1580
 #4 0x7fe6b26c64ed in g_markup_parse_context_parse (/usr/lib/x86_64-linux-gnu/libglib-2.0.so.0+0x694ed) (BuildId: 2426ee6f828a10bf4da963a4c474752c33742359)
 #5 0x5609bfa84339 in flatpak_xml_parse ../src/builder-utils.c:1807
 #6 0x5609bf9e9c85 in builder_manifest_cleanup ../src/builder-manifest.c:2857
 #7 0x5609bf9c8153 in main ../src/builder-main.c:1036
 #8 0x7fe6b1861600 (/usr/lib/x86_64-linux-gnu/libc.so.6+0x2a600) (BuildId: 90ebd03ae9d9f42b23b4eb82fdf70352cf744198)
 #9 0x7fe6b1861717 in __libc_start_main (/usr/lib/x86_64-linux-gnu/libc.so.6+0x2a717) (BuildId: 90ebd03ae9d9f42b23b4eb82fdf70352cf744198)
 #10 0x5609bf983ca4 in _start (/__w/flatpak-builder/flatpak-builder/builddir/src/flatpak-builder+0x129ca4) (BuildId: 45a57fa0ad4e37aa0d3dc0183b8f3f5ad6f2a6b8)
SUMMARY: AddressSanitizer: 14 byte(s) leaked in 1 allocation(s).
```

Fixes: d9f5a5e7 ("manifest: Don't emit empty id in `provides` when renaming")

src/builder-manifest.c
@@ -2891,7 +2891,8 @@ builder_manifest_cleanup (BuilderManifest *self,
                       flatpak_xml_add (n_root, n_provides);
                     }
                   n_provides_id = flatpak_xml_new ("id");
-                  id_text = flatpak_xml_new_text (g_steal_pointer (&old_id));
+                  id_text = flatpak_xml_new_text (old_id);
+                  g_clear_pointer (&old_id, g_free);
                   flatpak_xml_add (n_provides_id, id_text);
                   flatpak_xml_add (n_provides, n_provides_id);
                 }